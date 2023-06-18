22d66ac82347a6cb59c2ae53b659d88c1e066dc9
singhakshay29 <akshay2898.as@gmail.com>
2023-06-18 15:28:27 +0530

    react

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,7 +1,72 @@
 import React from 'react'
-
+import './Portfolio.css'
 export default function Portfolio() {
   return (
-    <div>Portfolio</div>
+   <section id='portfolio'>
+    <h5>My Recent Work</h5>
+    <h2>Portfolio</h2>
+    <div className='container portfolio__container'>
+      <article className='portfolio__item'>
+        <div className='portfolio__item-image'>
+          <img src="img" alt='pic'/>
+        </div>
+        <h3>Portfolio Title Name</h3>
+       <div className="portfolio__item-cta">
+       <a href='#abc' className='btn' target='_blank'>Github</a>
+        <a href='#https' className='btn btn-primary' target='_blank'>Live Demo</a>
+       </div>
+      </article>
+      <article className='portfolio__item'>
+        <div className='portfolio__item-image'>
+          <img src="img" alt='pic'/>
+        </div>
+        <h3>Portfolio Title Name</h3>
+        <div className="portfolio__item-cta">
+       <a href='#abc' className='btn' target='_blank'>Github</a>
+        <a href='#https' className='btn btn-primary' target='_blank'>Live Demo</a>
+       </div>
+       </article>
+      <article className='portfolio__item'>
+        <div className='portfolio__item-image'>
+          <img src="img" alt='pic'/>
+        </div>
+        <h3>Portfolio Title Name</h3>
+        <div className="portfolio__item-cta">
+       <a href='#abc' className='btn' target='_blank'>Github</a>
+        <a href='#https' className='btn btn-primary' target='_blank'>Live Demo</a>
+       </div>
+       </article>
+      <article className='portfolio__item'>
+        <div className='portfolio__item-image'>
+          <img src="img" alt='pic'/>
+        </div>
+        <h3>Portfolio Title Name</h3>
+        <div className="portfolio__item-cta">
+       <a href='#abc' className='btn' target='_blank'>Github</a>
+        <a href='#https' className='btn btn-primary' target='_blank'>Live Demo</a>
+       </div>
+       </article>
+      <article className='portfolio__item'>
+        <div className='portfolio__item-image'>
+          <img src="img" alt='pic'/>
+        </div>
+        <h3>Portfolio Title Name</h3>
+        <div className="portfolio__item-cta">
+       <a href='#abc' className='btn' target='_blank'>Github</a>
+        <a href='#https' className='btn btn-primary' target='_blank'>Live Demo</a>
+       </div>
+       </article>
+      <article className='portfolio__item'>
+        <div className='portfolio__item-image'>
+          <img src="img" alt='pic'/>
+        </div>
+        <h3>Portfolio Title Name</h3>
+        <div className="portfolio__item-cta">
+       <a href='#abc' className='btn' target='_blank'>Github</a>
+        <a href='#https' className='btn btn-primary' target='_blank'>Live Demo</a>
+       </div>
+       </article>
+    </div>
+   </section>
   )
 }
--- src/components/services/Service.jsx
+++ src/components/services/Service.jsx
@@ -1,7 +1,73 @@
 import React from 'react'
+import './Service.css'
 
 export default function Service() {
   return (
-    <section id='service'>Service</section>
+    <section id='service'>
+      <h5>What I offer</h5>
+      <h2>Services</h2>
+      <div className='container services__container'>
+        <article className='service'>
+          <div className='service__head'>
+            <h3>Resposive Design</h3>
+          </div>
+
+          <ul className='service__list'>
+            <li>
+              <p>Responsive design refers to a web design approach that aims to create websites or applications. The goal is to ensure that the content and layout of a website automatically adjust to accommodate different screen resolutions, orientations, and devices, such as desktop computers, laptops, tablets, and mobile phones.
+                Key principles and techniques involved in responsive design include: Flexible Images,Media Queries,Mobile-First Approach
+              </p>
+            </li>
+          </ul>
+        </article>
+        <article className='service'>
+          <div className='service__head'>
+            <h3>Web Design</h3>
+          </div>
+
+          <ul className='service__list'>
+            <li>
+              <p>
+              I'm skilled in using the Bootstrap framework to create responsive and visually appealing websites.
+
+Once the design is finalized, the web designer works closely with web developers to bring the design to life using coding languages like HTML, CSS, and JavaScript.
+              </p>
+            </li>
+          </ul>
+        </article>
+        <article className='service'>
+          <div className='service__head'>
+            <h3>Web Devlopment</h3>
+          </div>
+
+          <ul className='service__list'>
+            <li>
+              <p>
+              I can design and develop web applications while ensuring that they are optimized for usability and performance.
+
+Key principles and techniques involved in responsive design include: Flexible Images,Media Queries,Mobile-First Approach
+              </p>
+            </li>
+          </ul>
+        </article>
+        <article className='service'>
+          <div className='service__head'>
+            <h3>Clean Coder</h3>
+          </div>
+
+          <ul className='service__list'>
+            <li>
+              <p>
+              Clean code refers to writing code that is easy to read, understand, and maintain.
+
+I choose meaningful names for variables, functions, and classes that accurately describe their purpose and functionality.
+              </p>
+             </li>
+          </ul>
+        </article>
+      </div>
+
+
+    </section>
   )
 }
