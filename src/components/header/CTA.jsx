afb5a2a74b2878bfc8e78f1de024e6843a92c5c9
singhakshay29 <akshay2898.as@gmail.com>
2023-12-10 19:07:16 +0530

    Updates

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,14 +1,18 @@
-import React from 'react'
-import cv from '../../assets/cv.pdf'
+import React from "react";
+import cv from "../../assets/cv.pdf";
 
 const CTA = () => {
   return (
-    <div className='cta'>
-        <a href={cv} download className='btn'>Download CV</a>
-        <a href="#contacts" className='btn btn-primary'>Contact Me</a>
-        <br></br>
+    <div className="cta">
+      <a href={cv} download className="btn">
+        Download CV
+      </a>
+      <a href="#contacts" className="btn btn-primary">
+        Contact Me
+      </a>
+      <br></br>
     </div>
-  )
-}
+  );
+};
 
-export default CTA
+export default CTA;
--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,46 +1,118 @@
-import React from 'react'
-import './Portfolio.css'
-import project1 from '../../assets/project1.png'
-import project2 from '../../assets/Project2.png'
-import project3 from '../../assets/project3.png'
+import React from "react";
+import "./Portfolio.css";
+import soul from "../../assets/soul.png";
+import amazon from "../../assets/amazon.png";
+import project1 from "../../assets/project1.png";
+import project2 from "../../assets/Project2.png";
+import project3 from "../../assets/project3.png";
 
 export default function Portfolio() {
   return (
-   <section id='portfolio'>
-    <h5>My Recent Work</h5>
-    <h2>Portfolio</h2>
-    <div className='container portfolio__container'>
-      <article className='portfolio__item'>
-        <div className='portfolio__item-image'>
-          <img src={project1} alt='pic'/>
-        </div>
-        <h3>Home Page Layout</h3>
-        <div className="portfolio__item-cta">
-       <a href='https://github.com/singhakshay29/home-page-layout' className='btn'>Github</a>
-        <a href='https://singhakshay29.github.io/home-page-layout/' className='btn btn-primary'>Live Demo</a>
-       </div>
-       </article>
-      <article className='portfolio__item'>
-        <div className='portfolio__item-image'>
-          <img src={project2} alt='pic'/>
-        </div>
-        <h3>Movie Deck</h3>
-        <div className="portfolio__item-cta">
-       <a href='https://github.com/singhakshay29/HTML-and-CSS-for-Movie-Deck---JS-currying-Debouncing-JS-Project-Building-Sessi---gbv4p6pdeupf' className='btn'>Github</a>
-        <a href='https://singhakshay29.github.io/HTML-and-CSS-for-Movie-Deck---JS-currying-Debouncing-JS-Project-Building-Sessi---gbv4p6pdeupf/' className='btn btn-primary'>Live Demo</a>
-       </div>
-       </article>
-      <article className='portfolio__item'>
-        <div className='portfolio__item-image'>
-          <img src={project3} alt='pic'/>
-        </div>
-        <h3>YourDictionary</h3>
-        <div className="portfolio__item-cta">
-       <a href='https://github.com/singhakshay29/YourDictionary' className='btn'>Github</a>
-        <a href='https://singhakshay29.github.io/YourDictionary/' className='btn btn-primary'>Live Demo</a>
-       </div>
-       </article>
-    </div>
-   </section>
-  )
+    <section id="portfolio">
+      <h5>My Recent Work</h5>
+      <h2>Portfolio</h2>
+      <div className="container portfolio__container">
+        <article className="portfolio__item">
+          <div className="portfolio__item-image">
+            <img src={soul} alt="pic" />
+          </div>
+          <h3>The Souled Store</h3>
+          <div className="portfolio__item-cta">
+            <a
+              href="https://github.com/singhakshay29/the_soul_store"
+              className="btn"
+            >
+              Github
+            </a>
+            <a
+              href="https://the-soul-store.vercel.app/"
+              className="btn btn-primary"
+            >
+              Live Demo
+            </a>
+          </div>
+        </article>
+        <article className="portfolio__item">
+          <div className="portfolio__item-image">
+            <img src={amazon} alt="pic" />
+          </div>
+          <h3>Amazon music</h3>
+          <div className="portfolio__item-cta">
+            <a
+              href="https://github.com/singhakshay29/amazonmusic"
+              className="btn"
+            >
+              Github
+            </a>
+            <a
+              href="https://amazonmusic.vercel.app/"
+              className="btn btn-primary"
+            >
+              Live Demo
+            </a>
+          </div>
+        </article>
+        <article className="portfolio__item">
+          <div className="portfolio__item-image">
+            <img src={project1} alt="pic" />
+          </div>
+          <h3>Home Page Layout</h3>
+          <div className="portfolio__item-cta">
+            <a
+              href="https://github.com/singhakshay29/home-page-layout"
+              className="btn"
+            >
+              Github
+            </a>
+            <a
+              href="https://singhakshay29.github.io/home-page-layout/"
+              className="btn btn-primary"
+            >
+              Live Demo
+            </a>
+          </div>
+        </article>
+        <article className="portfolio__item">
+          <div className="portfolio__item-image">
+            <img src={project2} alt="pic" />
+          </div>
+          <h3>Movie Deck</h3>
+          <div className="portfolio__item-cta">
+            <a
+              href="https://github.com/singhakshay29/HTML-and-CSS-for-Movie-Deck---JS-currying-Debouncing-JS-Project-Building-Sessi---gbv4p6pdeupf"
+              className="btn"
+            >
+              Github
+            </a>
+            <a
+              href="https://singhakshay29.github.io/HTML-and-CSS-for-Movie-Deck---JS-currying-Debouncing-JS-Project-Building-Sessi---gbv4p6pdeupf/"
+              className="btn btn-primary"
+            >
+              Live Demo
+            </a>
+          </div>
+        </article>
+        <article className="portfolio__item">
+          <div className="portfolio__item-image">
+            <img src={project3} alt="pic" />
+          </div>
+          <h3>YourDictionary</h3>
+          <div className="portfolio__item-cta">
+            <a
+              href="https://github.com/singhakshay29/YourDictionary"
+              className="btn"
+            >
+              Github
+            </a>
+            <a
+              href="https://singhakshay29.github.io/YourDictionary/"
+              className="btn btn-primary"
+            >
+              Live Demo
+            </a>
+          </div>
+        </article>
+      </div>
+    </section>
+  );
 }
